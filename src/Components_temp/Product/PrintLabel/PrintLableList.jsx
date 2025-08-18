d4810a117efc9419e3a9c73ff7a0787c1dfcdea2
Guhan <guhanntamil@gmail.com>
2025-08-18 12:35:55 +0530

    finish the overall style of the product component

--- src/Components_temp/Product/PrintLabel/PrintLableList.jsx
+++ src/Components_temp/Product/PrintLabel/PrintLableList.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import "./PrintLableList.css";
+// import "./PrintLableList.css";
 import "../Product.css"; // Common product styles
 
 function PrintLabel() {
